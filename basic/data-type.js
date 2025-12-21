62f29025d541f40270047d722afbde5849506034
Jadob56 <127203194+Jadob56@users.noreply.github.com>
2025-12-21 11:48:07 +0000

    config

--- basic/data-type.js
+++ basic/data-type.js
@@ -1,2 +1,30 @@
-let Name = "jadob"
-console.log(Name);
+// Primative//
+/*const Name = "jadob"
+const age = 25;
+let idName = false;
+const userID = null;
+let password;
+
+let isActive = Symbol("active");
+
+console.log(typeof Name);
+console.log(typeof age);
+console.log(typeof idName);
+console.log(typeof userID);
+console.log(typeof password);
+console.log(typeof isActive);*/
+
+//Non-primetive//
+const hobbies = ['reading','coding','gaming']
+const obj={
+    firstName:"jadob",
+    Lastname:"shikder",
+    age:25
+}
+const name = function() {
+    console.log("hello jadob");
+    
+}
+console.log(typeof hobbies);
+console.log(typeof obj);
+console.log(typeof name);
