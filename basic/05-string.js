d100a516893c09bc6d6235e32276e7b60f9d8139
Jadob56 <127203194+Jadob56@users.noreply.github.com>
2025-12-25 13:13:41 +0000

    useit

--- basic/05-string.js
+++ basic/05-string.js
@@ -1,3 +1,15 @@
 const myName = "Jadob"
 const age = '25'
-console.log(`My name is ${myName} and I am ${age} years old`)
+//console.log(`My name is ${myName} and I am ${age} years old`)//
+
+const gameName = "AroundWorld";
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf("W"));
+console.log(gameName.includes("World"));
+console.log(gameName.slice(0, 6));
+console.log(gameName.replace("World", "Universe"));
+
+
