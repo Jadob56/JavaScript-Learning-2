3ec6949b73060b824a8004598cfc49e7b212cf10
Jadob56 <127203194+Jadob56@users.noreply.github.com>
2026-01-19 05:46:29 +0000

    new file

---
+++ basic/01-variable.js
@@ -0,0 +1,11 @@
+const accountName = "Pabitra"
+let accountEmail ="jad2113@gmail.com"
+var accountPassword ="123456"
+Mycity ="Dhaka"
+
+// accountName = "Jad"
+accountEmail = "jadon23@pabitrea"
+accountPassword = "654321"
+Mycity = "Chittagong"
+console.table({accountName, accountEmail, accountPassword, Mycity});
+console.log(accountName);
