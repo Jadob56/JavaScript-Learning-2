2af009071943f1f24798cbaf147349dd4d9714c6
Jadob56 <127203194+Jadob56@users.noreply.github.com>
2026-01-22 04:39:42 +0000

    operationconversion

---
+++ basic/03-ConversionOperation.js
@@ -0,0 +1,29 @@
+let nameIs = "Jadob"
+//let conversionNumber = Number(nameIs)
+//console.log(conversionNumber);
+//console.log(typeof conversionNumber);
+
+let ageIs = 25
+let conversionString = String(ageIs)
+//console.log(conversionString);
+//console.log(typeof conversionString);
+
+let isMember = false
+let conversionBoolean = Boolean(isMember) 
+//console.log(conversionBoolean);
+//console.log(typeof conversionBoolean);
+
+let score = "0"
+let conversionNumber = Number(score)
+console.log(conversionNumber);
+console.log(typeof conversionNumber);
+
+//"33">= 33
+//0>= false
+//1>= true
+//"">= 0
+//"Jadob">= NaN
+//undefined>= NaN
+//null>=0
+//"0">=0
+
