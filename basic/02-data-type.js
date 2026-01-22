0044a298c00baf3dd203da87f235bd34d1a7f126
Jadob56 <127203194+Jadob56@users.noreply.github.com>
2026-01-22 04:13:13 +0000

    02-data-type update

---
+++ basic/02-data-type.js
@@ -0,0 +1,12 @@
+//String >=""
+//bigint >=123n
+//boolean >= true/false
+//null >= null
+//undefined >= undefined
+//number >= 123
+//symbol >= Symbol("id")
+
+//object >= {}
+//array >= []
+//function >= function() {}
+
